e677995caca38b96649ebae73eef650792f2a006
Alex <climie.alex@gmail.com>
2025-01-03 23:01:13 +0800

    data

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,9 +4,14 @@ import file from '../components/file.vue'
 
 <template>
   <main>
-    <file fileName="fortnite.md" date="2024/12">
-      <h1>hi! I'm alex, a high school student.</h1>
-      <p>I love</p>
+    <file fileName="topdownuniverse.md" date="2024/12">
+      <p>imagine the universe, but we ate a dimension. tasty.</p>
+    </file>
+    <file fileName="nomissmayhem.md" date="2024/11">
+      <p>counterspell hong kong!!! top 10 global finalist!!! et cetera</p>
+    </file>
+    <file fileName="lionel3.md" date="2024/6">
+      <p>i don't like lionel, made my own</p>
     </file>
   </main>
 </template>
