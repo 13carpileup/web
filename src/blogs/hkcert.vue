6835a1c9223f1e939adba82bdfa5a6269ae66647
Alex <climie.alex@gmail.com>
2025-01-04 22:16:04 +0800

    forgo to add files

---
+++ src/blogs/hkcert.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import file from '../components/file.vue'
+import blog from '../components/blog.vue'
+
+</script>
+
+<template>
+    <file fileName="hkcert.md" date="2024/10">
+        <blog src="test.markdown"></blog>
+    </file>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/blog.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import VueMarkdown from "vue-markdown-render"; 
+
+let props = defineProps<{
+  src: string; 
+}>();
+
+let url = "../src/assets/posts/" + props.src;
+
+const markdownContent = ref<string>(""); 
+
+onMounted(async () => {
+  try {
+    const response = await fetch(url); 
+    if (response.ok) {
+      markdownContent.value = await response.text(); 
+      console.log(markdownContent.value)
+    } else {
+      console.error(`Failed to load markdown file at ${url}`);
+    }
+  } catch (error) {
+    console.error("Error loading markdown file:", error);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <VueMarkdown :source="markdownContent" />
+  </div>
+</template>
+
+
+<style>
+code {
+    width: 100%;
+    height:100%;
+}
+</style>
